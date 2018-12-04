a96e4ddd5273018c07d09e0b12045390c330f973
Vaughn Sander <V.Sander@ET.CO.ZA>
2018-12-04 10:59:28 +0200

    werk

---
+++ Logger.py
@@ -0,0 +1,32 @@
+# =====================================================================
+# ET Nice 01/12/2018
+# By Vaughn Sander
+# This script Uses a Raspberry Pi ZeroW to long the triggers on Beams.
+# =====================================================================
+
+#!/usr/bin/env python
+
+import time
+import RPI.GPIO as GPIO
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(4, GPIO.OUT)
+
+Start_Time = time.time()
+
+while True:
+    GPIO.output(4, True)
+    time.sleep(1)
+    GPIO.output(4,False)
+    time.sleep(1)
+
+
+
+#End_Time = time.time()
+
+#Delta = End_Time - Start_Time
+#print(Start_Time)
+#print(End_Time)
+#print(Delta)
+
---
+++ README.md
@@ -0,0 +1,2 @@
+# ET-Logger
+Raspberry pi Zero Logger 
